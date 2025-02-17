4881d520f7cb6ec2d090319b93b475ac4df86288
Aleksandar Prodanovic <aleksandar_prod18@hotmail.com>
2025-02-17 19:41:38 +0100

    Improve responsiveness for mobile devices and center text content

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <div className="w-[70%] mb-5 bg-white bg-opacity-90 shadow-lg rounded-md">
+      <div className="w-full sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] mb-5 bg-white bg-opacity-90 shadow-lg rounded-md">
         <div className="h-80 flex flex-col items-center justify-center p-6">
-          <h1 className="text-2xl font-bold text-gray-700 text-center">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-700 text-center">
             Welcome to Stampfolio
           </h1>
-          <p className="text-base mt-2 text-gray-600 text-center">
+          <p className="text-base sm:text-lg md:text-xl mt-2 text-gray-600 text-center">
             <strong>Stampfolio</strong> is a platform for stamp collectors to
             share, organize, and explore beautiful stamps from around the world.
             Whether you&apos;re a passionate philatelist or just starting your
