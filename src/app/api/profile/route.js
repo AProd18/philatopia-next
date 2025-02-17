6b67ce22ad2c0c58f451b6680301ab6e0378598c
Aleksandar Prodanovic <aleksandar_prod18@hotmail.com>
2025-02-17 11:52:20 +0100

    Implemented profile page with: - Profile image upload (using Sharp for processing) - Convert uploaded images to WebP and reduce file size - About Me section (editable and stored in DB) - API routes for fetching and updating profile data

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -3,6 +3,7 @@ import prisma from "../../../lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 import { supabaseAdmin } from "../../../lib/supabase";
+import sharp from "sharp";
 
 // Handle GET request to fetch user profile details
 export async function GET(request) {
@@ -59,6 +60,18 @@ export async function POST(request) {
 
   try {
     let imageUrl = null;
+    let oldImagePath = null;
+
+    // Find the old image from the database before the update
+    const existingUser = await prisma.user.findUnique({
+      where: { email: userEmail },
+      select: { profileImage: true },
+    });
+
+    if (existingUser?.profileImage) {
+      const parts = existingUser.profileImage.split("/");
+      oldImagePath = parts.slice(-2).join("/"); // Get the path to the old image
+    }
 
     // If a new profile image is provided, upload it to Supabase storage
     if (image) {
@@ -66,9 +79,16 @@ export async function POST(request) {
       const fileName = `${userEmail}-${Date.now()}.${fileExt}`;
       const filePath = `profile_images/${fileName}`;
 
+      // Convert the image to WebP format and reduce its size to max 300x300px
+      const buffer = await image.arrayBuffer();
+      const optimizedImage = await sharp(Buffer.from(buffer))
+        .resize(300, 300, { fit: "cover" }) // Change image size
+        .toFormat("webp") // Convert to WeBP format
+        .toBuffer();
+
       const { data, error } = await supabaseAdmin.storage
         .from(process.env.SUPABASE_BUCKET_NAME)
-        .upload(filePath, image, { contentType: image.type });
+        .upload(filePath, optimizedImage, { contentType: "image/webp" });
 
       if (error) {
         console.error("Error uploading to Supabase:", error);
@@ -84,6 +104,13 @@ export async function POST(request) {
         .getPublicUrl(filePath);
 
       imageUrl = publicUrlData.publicUrl;
+
+      // Delete old image from SUpabase Storage (if exist)
+      if (oldImagePath) {
+        await supabaseAdmin.storage
+          .from(process.env.SUPABASE_BUCKET_NAME)
+          .remove([oldImagePath]);
+      }
     }
 
     // Update user profile in the database (aboutMe & profile image if available)
