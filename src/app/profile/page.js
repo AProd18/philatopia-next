ace8cd69edbf091bc44cd4acdea624ef49fba7cc
Aleksandar Prodanovic <aleksandar_prod18@hotmail.com>
2025-04-02 20:41:14 +0200

    Add opacity on profile card

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -61,7 +61,7 @@ export default function Profile() {
   };
 
   return (
-    <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md">
+    <div className="max-w-2xl mx-auto p-6 opacity-90 bg-white rounded-lg shadow-md">
       <h1 className="text-2xl font-bold mb-6 text-gray-500">Profile</h1>
 
       <div className="mb-6 text-center">
