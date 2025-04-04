9d60bc7573077f08c415fd73de3ff5918a4301ec
Aleksandar Prodanovic <aleksandar_prod18@hotmail.com>
2025-04-04 19:00:05 +0200

    feat: display total number of users and stamps on homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
+// app/page.js
 import Link from "next/link";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const totalUsers = await prisma.user.count();
+  const totalStamps = await prisma.stamp.count();
+
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
+    <main className="flex flex-col items-center justify-center py-10">
+      {/* Welcome Section */}
       <div className="w-full sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] mb-5 bg-white bg-opacity-90 shadow-lg rounded-md">
         <div className="h-80 flex flex-col items-center justify-center p-6">
           <h1 className="text-2xl sm:text-3xl md:text-3xl font-bold text-gray-700 text-center">
@@ -21,7 +27,7 @@ export default function Home() {
         </div>
       </div>
 
-      {/* New Announcement Section */}
+      {/* Announcement Section */}
       <div className="w-full sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] mb-5 bg-red-100 bg-opacity-90 shadow-lg rounded-md">
         <div className="h-60 flex flex-col items-center justify-center p-6">
           <h2 className="text-2xl sm:text-3xl md:text-2xl font-bold text-red-700 text-center">
@@ -35,21 +41,16 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-full sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] bg-blue-100 bg-opacity-90 shadow-lg rounded-md">
-        <div className="h-60 flex flex-col items-center justify-center p-6">
-          <h2 className="text-2xl sm:text-3xl md:text-2xl font-bold text-blue-700 text-center">
-            Explore the Philately Gallery
-          </h2>
-          <p className="text-base sm:text-lg md:text-sm mt-2 text-blue-600 text-center">
-            Browse a wide variety of stamps uploaded by our community. No
-            account needed!
+      {/* Stats Section */}
+      <div className="w-full sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] mb-5 bg-white bg-opacity-90 shadow-md rounded-md">
+        <div className="p-6 flex flex-col items-center">
+          <h3 className="text-xl font-semibold text-gray-700 mb-2">
+            📊 Stampfolio Stats
+          </h3>
+          <p className="text-gray-600 text-center">
+            Total Users: <span className="font-bold">{totalUsers}</span> | Total
+            Stamps: <span className="font-bold">{totalStamps}</span>
           </p>
-          <Link
-            href="/philately-gallery"
-            className="mt-4 bg-blue-700 text-white px-6 py-2 rounded hover:bg-blue-800 transition"
-          >
-            View Gallery
-          </Link>
         </div>
       </div>
     </main>
