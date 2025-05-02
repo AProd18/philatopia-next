513e93874482280b30f227f205e463e894929d7a
Aleksandar Prodanovic <aleksandar_prod18@hotmail.com>
2025-05-02 12:18:30 +0200

    style: redesign sort by component

--- src/components/SortDropdown.js
+++ src/components/SortDropdown.js
@@ -3,14 +3,17 @@
 export default function SortDropdown({ value, onChange }) {
   return (
     <div className="bg-white w-full  shadow-md rounded p-4 mb-6 border border-gray-300 opacity-80">
-      <label htmlFor="sort" className="mr-2 font-medium text-gray-700">
+      <label
+        htmlFor="sort"
+        className="block text-sm font-semibold text-gray-700 mb-2"
+      >
         Sort by:
       </label>
       <select
         id="sort"
         value={value}
         onChange={(e) => onChange(e.target.value)}
-        className="border rounded px-3 py-2"
+        className="block w-full px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
       >
         <option value="date-newest">Date Added (Newest)</option>
         <option value="date-oldest">Date Added (Oldest)</option>
