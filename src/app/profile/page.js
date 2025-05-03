82b9c2df93bf182ae2c66b54010334bf736e27b2
Aleksandar Prodanovic <aleksandar_prod18@hotmail.com>
2025-05-03 16:01:54 +0200

    Adjust profile page layout: added vertical spacing to better center content

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -61,7 +61,7 @@ export default function Profile() {
   };
 
   return (
-    <div className="max-w-2xl mx-auto p-6 opacity-90 bg-white rounded-lg shadow-md ">
+    <div className="max-w-2xl mx-auto p-6 opacity-90 bg-white rounded-lg shadow-md mt-36">
       <div className="mb-6 text-center">
         <div className="relative w-32 h-32 mx-auto">
           {profileImage ? (
