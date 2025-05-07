10ed3f34d400a684508850da81ae9e4bfae573e2
Aleksandar Prodanovic <aleksandar_prod18@hotmail.com>
2025-05-07 20:32:55 +0200

    feat: implement dark/light mode support to CountryFilter, SortDropdown, and SearchBar components on Stamps page

--- src/components/CountryFilter.js
+++ src/components/CountryFilter.js
@@ -21,10 +21,10 @@ const CountryFilter = ({ stamps, onFilter }) => {
   };
 
   return (
-    <div className="bg-white w-full  shadow-md rounded p-4 mb-6 border border-gray-300 opacity-80">
+    <div className="w-full shadow-md rounded p-4 mb-6 border border-gray-300 dark:border-gray-600 bg-white dark:bg-[rgba(0,0,0,0.4)]">
       <label
         htmlFor="country"
-        className="block text-sm font-semibold text-gray-700 mb-2"
+        className="block text-sm font-semibold text-gray-700 dark:text-gray-200 mb-2"
       >
         Filter by Country
       </label>
@@ -32,7 +32,7 @@ const CountryFilter = ({ stamps, onFilter }) => {
         id="country"
         value={selectedCountry}
         onChange={handleChange}
-        className="block w-full px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+        className="block w-full px-3 py-2 text-sm border border-gray-300 dark:border-gray-600 rounded-md shadow-sm bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
       >
         <option value="">All Countries</option>
         {uniqueCountries.map((country) => (
--- src/components/SortDropdown.js
+++ src/components/SortDropdown.js
@@ -2,10 +2,10 @@
 
 export default function SortDropdown({ value, onChange }) {
   return (
-    <div className="bg-white w-full  shadow-md rounded p-4 mb-6 border border-gray-300 opacity-80">
+    <div className="w-full shadow-md rounded p-4 mb-6 border border-gray-300 dark:border-gray-600 bg-white dark:bg-[rgba(0,0,0,0.4)]">
       <label
         htmlFor="sort"
-        className="block text-sm font-semibold text-gray-700 mb-2"
+        className="block text-sm font-semibold text-gray-700 dark:text-gray-200 mb-2"
       >
         Sort by:
       </label>
@@ -13,7 +13,7 @@ export default function SortDropdown({ value, onChange }) {
         id="sort"
         value={value}
         onChange={(e) => onChange(e.target.value)}
-        className="block w-full px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+        className="block w-full px-3 py-2 text-sm border border-gray-300 dark:border-gray-600 rounded-md shadow-sm bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
       >
         <option value="date-newest">Date Added (Newest)</option>
         <option value="date-oldest">Date Added (Oldest)</option>
