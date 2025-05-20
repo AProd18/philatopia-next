fb0fdf621a5b466a671afe810f4db03049821422
Aleksandar Prodanovic <aleksandar_prod18@hotmail.com>
2025-05-20 23:45:51 +0200

    feat: Add dark/light theme support to UserProfileModal

--- src/components/UserProfileModal.js
+++ src/components/UserProfileModal.js
@@ -8,13 +8,13 @@ const UserProfileModal = ({ user, onClose }) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
       <div
-        className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-md"
+        className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm"
         onClick={onClose}
       ></div>
 
-      <div className="relative bg-white p-6 rounded-lg shadow-lg max-w-sm w-full z-10 text-center">
+      <div className="relative z-10 w-full max-w-sm rounded-lg p-6  shadow-lg bg-white text-gray-800 dark:bg-gray-900 dark:text-gray-100   text-center">
         <button
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
+          className="absolute top-2 right-2 text-gray-500 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white"
           onClick={onClose}
         >
           ✖
@@ -29,14 +29,14 @@ const UserProfileModal = ({ user, onClose }) => {
             className="rounded-full mx-auto mb-4 object-cover w-24 h-24"
           />
         ) : (
-          <div className="w-24 h-24 mx-auto mb-4 rounded-full bg-gray-300 flex items-center justify-center">
+          <div className="w-24 h-24 mx-auto mb-4 rounded-full bg-gray-300 dark:bg-gray-700 flex items-center justify-center text-gray-600 dark:text-gray-300">
             No Image
           </div>
         )}
-        <h2 className="text-lg font-semibold text-gray-600">
+        <h2 className="text-lg font-semibold text-gray-600 dark:text-gray-300">
           {user.name || "Unknown User"}
         </h2>
-        <p className="text-sm text-gray-600 mt-2 whitespace-pre-line">
+        <p className="text-sm text-gray-600 mt-2 whitespace-pre-line dark:text-gray-300">
           {user.aboutMe || "No bio available."}
         </p>
       </div>
